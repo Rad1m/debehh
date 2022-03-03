824dd18559ea260f18d15a7a0fc3e433cd41eabd
Rad1m <radim.simanek@protonmail.ch>
2022-03-03 13:05:29 +0000

    html

--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -2,12 +2,24 @@ import React, { useEffect, useState } from "react";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+import TokenArtifact from "../contracts/DeBeToken.json";
+import TokenAddress from "../contracts/token-address.json";
+import { ethers } from "ethers";
 
 declare var window: any;
 const ethereum = window.ethereum as MetaMaskInpageProvider;
 
 async function connectWallet() {
   await ethereum.request({ method: "eth_requestAccounts" });
+}
+
+async function execute() {
+  const contractAddress = TokenAddress.Token;
+  const abi = TokenArtifact.abi;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  await contract.balanceOf(signer);
 }
 
 export function ConnectWalletButton() {
