f4633e6c82099f1a9070bf5de3ce54dea4ef654d
Rad1m <radim.simanek@protonmail.ch>
2022-03-03 07:57:35 +0000

    metamask buton

--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { MetaMaskInpageProvider } from "@metamask/providers";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+
+declare var window: any;
+const ethereum = window.ethereum as MetaMaskInpageProvider;
+
+async function connectWallet() {
+  await ethereum.request({ method: "eth_requestAccounts" });
+}
 
 export function ConnectWalletButton() {
   return (
@@ -10,11 +18,11 @@ export function ConnectWalletButton() {
           variant="contained"
           color="secondary"
           sx={{ mx: "auto", width: 200, p: 1, m: 1 }}
+          onClick={() => connectWallet()}
         >
           Connect
         </Button>
       </Tooltip>
-      {/* Disconnect metamsak */}
     </>
   );
 }
